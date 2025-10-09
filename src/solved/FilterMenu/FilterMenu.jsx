e659fdeeb750c109684abd0dc9a27255a2bc99e9
Shambhavi Sivan <shambhavisivan@Shambhavis-MacBook-Pro.local>
2025-10-09 21:37:58 +0530

    ex-integrate-usemenu-2

--- src/solved/FilterMenu/FilterMenu.jsx
+++ src/solved/FilterMenu/FilterMenu.jsx
@@ -80,6 +80,8 @@ const MenuWithOverlay = () => {
       */}
       <button
         type="button"
+        {...menuState.trigger}
+        onClick={menuState.toggle}
         style={{ width: "400px" }}
         className="md:w-auto px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 accessible-focus"
       >
