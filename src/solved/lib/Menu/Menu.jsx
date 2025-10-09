767803f8adfd76c33b256712ce2a9725ef9937eb
Shambhavi Sivan <shambhavisivan@Shambhavis-MacBook-Pro.local>
2025-10-09 21:20:24 +0530

    add ex-menu-overlay-5

--- src/solved/lib/Menu/Menu.jsx
+++ src/solved/lib/Menu/Menu.jsx
@@ -42,20 +42,6 @@ const Menu = ({
   ...props
 }) => {
   return (
-    /*
-      TODO: Wrap Overlay around MenuList
-
-      Props to pass to Overlay:
-      - trigger: Trigger element ref and id
-      - body: Overlay ref, visibility, placement, styles
-      - setVisible: Control overlay visibility
-      - close: Close overlay function
-      - open: Open overlay function
-      - toggle: Toggle overlay function
-      - pattern: "menu" (overlay type)
-      - autoFocus: false (for menus). you might require eslint-disable-next-line jsx-a11y/no-autofocus
-      - closeOnOutsideClick: true (close on outside click)
-    */
     <Overlay
       trigger={trigger}
       body={body}
