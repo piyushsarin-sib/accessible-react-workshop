1af727421c23cff51479e7186e4ce8228df78243
piyushsarin-sib <piyush.sarin@sendinblue.com>
2025-10-13 19:44:11 +0530

    fix: handle tag

--- scripts/use-full.sh
+++ scripts/use-full.sh
@@ -1,5 +1,8 @@
 # pkill -f "Visual Studio Code"
 sudo chown -R $(whoami) .
+#delete all local tags and branches
+git branch --merged | grep -v "^\*" | xargs git branch -d
+git tag | xargs git tag -d 
 # chmod +x scripts/list-commits.sh
 # scripts/list-commits.sh
 chmod +x scripts/create-all-card-tags.sh
