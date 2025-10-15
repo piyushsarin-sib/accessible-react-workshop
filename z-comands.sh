d448206d42d7eac591d867adf8b842f404b05d5c
piyushsarin-sib <piyush.sarin@sendinblue.com>
2025-10-15 13:22:04 +0530

    fix: added commands

--- z-comands.sh
+++ z-comands.sh
@@ -1,13 +1,34 @@
-#card Wrapper
+#Card Wrapper
 git exdiff ex1 step0
 
-
+#Product List
 git exdiff ex2 step0
 
-git exdiff ex3 step0
+# Overlays & Modals
+
+#"Menu"
+git exdiff ex3a step0
+
+#"FilterMenu"
+git exdiff ex3b step0
+
+#"useKeyboardHandlers"
+git exdiff ex3c step0
+
 
 #edge Cases
 git exdiff ex4 step0
+
+
+#Extra
+
+# To kill the VSCode process
+pkill -f "Visual Studio Code"
+
+
+# Setup github alias
+chmod +x scripts/setup-git-aliases.sh
+scripts/setup-git-aliases.sh
 
 git next
 
